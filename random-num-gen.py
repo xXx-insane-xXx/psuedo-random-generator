be1aee1c2687d5bbdba6ef9375b08792bb64b6e6
xXxinsanexXx <shreyanshsaha19@proton.me>
2024-03-20 09:52:50 +0530

    (#001) Initial Commit

---
+++ random-num-gen.py
@@ -0,0 +1,17 @@
+import math
+import time
+num = []
+
+x = float(input("Enter start value : "))
+y = float(input("Enter end value : "))
+
+i = x
+
+while (i < y):
+    num.append(round(i, 2))
+    i += 0.1
+
+
+i <= range(x, y)*time.time()*1000 <= y
+
+print(num)
